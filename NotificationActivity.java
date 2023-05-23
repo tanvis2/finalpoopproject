6e02f3d8e3be658f135659ca42e2f4a2a1b1af00
RitikaKrishnakumar <131980646+RitikaKrishnakumar@users.noreply.github.com>
2023-05-22 20:11:34 -0400

    Create NotificationActivity.java

---
+++ NotificationActivity.java
@@ -0,0 +1,14 @@
+package edu.ncssm.krishnakumar24r.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+    }
+}
